edb2cf20e5ce96036cbaaab33be5bb076e719394
Logan Barnett <logustus@gmail.com>
2022-07-25 12:34:58 -0700

    make sure the Gemfile is also bumped
    Without being bumped, the Gemfile will fail to `bundle install` properly
    later, and this breaks automated builds.

--- release.rb
+++ release.rb
@@ -5,7 +5,10 @@ require_relative './version'
 version = "0.#{version_get().to_i + 1}.0"
 puts "Creating and pushing tag '#{version}'."
 version_set(version)
-puts `git add CHANGELOG.org lib/version.rb`
+puts `bundle install`
+# Gemfile.lock is also bumped after this, and needs to be added or it will foul
+# up later releases.
+puts `git add CHANGELOG.org lib/version.rb Gemfile.lock`
 puts `git commit -m "v#{version}"`
 # TODO: -a is unsigned (annotated) tagging, use signed tagging.
 puts `git tag "v#{version}"`
