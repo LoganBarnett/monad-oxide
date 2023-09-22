af42374e13bea1a2b2c2e58c5e06f57c89581b93
Logan Barnett <logustus@gmail.com>
2023-09-22 16:00:38 -0700

    try to fix unbumped Gemfile.lock issue

--- release.rb
+++ release.rb
@@ -5,7 +5,9 @@ require_relative './version'
 version = "0.#{version_get().to_i + 1}.0"
 puts "Creating and pushing tag '#{version}'."
 version_set(version)
-puts `bundle install`
+# --no-deployment allows the Gemfile.lock to update. This isn't technical a
+# deployment (such as like deploying a Rails app), so this isn't too egregious.
+puts `bundle install --no-deployment`
 # Gemfile.lock is also bumped after this, and needs to be added or it will foul
 # up later releases.
 puts `git add CHANGELOG.org lib/version.rb Gemfile.lock`
