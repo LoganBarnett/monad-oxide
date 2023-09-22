a930ca0ab3e438b692ce1ad0340fd3bc8e500c5e
GHA <tip@gha>
2023-09-22 22:57:47 +0000

    v0.5.0

--- lib/version.rb
+++ lib/version.rb
@@ -3,5 +3,5 @@ module MonadOxide
   # This version is locked to 0.x.0 because semver is a lie and this project
   # uses CICD to push new versions. Any commits that appear on main will result
   # in a new version of the gem created and published.
-  VERSION='0.4.0'
+  VERSION='0.5.0'
 end
