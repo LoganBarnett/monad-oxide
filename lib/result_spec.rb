6ec505af6a9d7269e5903707ebcfa1b753b4af62
Logan Barnett <logustus@gmail.com>
2025-11-06 15:26:38 -0800

    add MonadOxide::Result.try
    This adds a convenience `try` function that allows easily entering a `Result`
    chain.

--- lib/result_spec.rb
+++ lib/result_spec.rb
@@ -27,4 +27,72 @@ describe MonadOxide::Result do
       expect(outer.flatten()).to(be(innermost))
     end
   end
+
+  describe('.try') {
+
+    context('with lambdas') {
+
+      it('converts a successful outcome into an Ok') {
+        r = MonadOxide::Result.try(->() {
+          return 0
+        })
+        expect(r).to(be_ok())
+      }
+
+      it('wraps the return value in the Ok') {
+        r = MonadOxide::Result.try(->() {
+          return 0
+        })
+        expect(r.unwrap()).to(be(0))
+      }
+
+      it('converts a raised error in to an Err') {
+        r = MonadOxide::Result.try(->() {
+          raise StandardError.new('foo')
+        })
+        expect(r).to(be_err())
+      }
+
+      it('wraps the raised error in the Err') {
+        r = MonadOxide::Result.try(->() {
+          raise StandardError.new('foo')
+        })
+        expect(r.unwrap_err().message()).to(eq('foo'))
+      }
+
+    }
+
+    context('with blocks') {
+
+      it('converts a successful outcome into an Ok') {
+        r = MonadOxide::Result.try() {
+          next 0
+        }
+        expect(r.unwrap()).to(be(0))
+      }
+
+      it('wraps the return value in the Ok') {
+        r = MonadOxide::Result.try() {
+          next 0
+        }
+        expect(r.unwrap()).to(be(0))
+      }
+
+      it('converts a raised error in to an Err') {
+        r = MonadOxide::Result.try() {
+          raise StandardError.new('foo')
+        }
+        expect(r).to(be_err())
+      }
+
+      it('wraps the raised error in the Err') {
+        r = MonadOxide::Result.try() {
+          raise StandardError.new('foo')
+        }
+        expect(r.unwrap_err().message()).to(eq('foo'))
+      }
+
+    }
+
+  }
 end
